18a194828e0e24f92e72874852d5a28523d71f41
brendan <crickets147@gmail.com>
2014-08-27 13:51:48 -0400

    Created intial Vagrant box that will: 1) Install Debian 7.6 2) Create private static ip for guest 3) Forward SSH keys to guest 4) Install all dependencies that we currently need (django, uwsgi, pip, etc) 5) Create base Django project called leagueoftrivia 6) Auto gen Django static/media content 7) Initialize uwsgi instance that communicates with leagueoftrivia django app 8) Setup nginx to listen to port 80 and communicate with leagueoftrivia uwsgi instance using a socket 9) Update permissions for vagrant/www-data 10) Add pidfile for leagueoftrivia uwsgi instance 11) Daemonize leagueoftrivia uwsgi instance 12) Add uwsgi_params

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dev
+sudo apt-get install -y vim
+sudo pip install uwsgi
+sudo pip install Django
+sudo apt-get install -y nginx
+sudo service nginx start
+
+sudo ln -fs /vagrant/leagueoftrivia/leagueoftrivia_nginx.conf /etc/nginx/sites-enabled/
+if [ -f /etc/nginx/sites-enabled/default ]; then
+    sudo rm /etc/nginx/sites-enabled/default
+fi
+if [ ! -f /var/log/uwsgi.log ]; then
+    sudo touch /var/log/uwsgi.log
+fi
+sudo chmod 666 /var/log/uwsgi.log
+sudo service nginx restart
+uwsgi --ini /vagrant/leagueoftrivia/leagueoftrivia_uwsgi.ini
